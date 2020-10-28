59b2c661c206920ed9a547d470b35be50a578f3c
Paul AYling <jimmyyukka@gmail.com>
2020-10-28 13:32:16 +0100

    Outputed text to browser "Hello root" when endpoint is hit

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-//Starter version of the main app.js file
+//finished version of the app.js file
 "use strict";
 const express = require("express");
 const app = express();
 const port = process.env.port || 4444;
+const things = require("./routes/things");
+
+app.use(express.json());
+app.use("/things", things);
+//use the things.js file to handle
+//endpoints that start with /things
 
 app.get("/", (req, res) => {
   //handle root
+  res.send("hello root");
 });
 
 app.listen(port, err => {
@@ -22,5 +29,5 @@ app.listen(port, err => {
  * "/ab*cd" - "/ab" + anything + "cd"
  * /a/ - RegExp: anything that contains "a"
  * /.*man$/ - RegExp: anything that ends with "man"
- *
+ * ^ - starts with
  */
